4cf0ad29f8359cd31cce53741efc200fed3fb37e
Khedmity <kedmty_portfolio@hotmail.com>
2025-12-10 12:58:37 +0200

    up

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -18,18 +18,19 @@ export default function Projects() {
         {projects_data.map((p, idx) => (
           <div
             key={idx}
-            className={`w-[300px] p-5 rounded-xl h-[530px] bg-[#cdccca40]`}
+            className={`w-[300px] p-5 rounded-xl h-[550px] bg-[#cdccca40]`}
           >
             <div className={`h-68 w-full rounded-lg relative overflow-hidden bg-white`}>
               <Image
                 src={`/${p.img}`}
                 alt={`${p.title}`}
                 fill
-                  className={idx !== projects_data.length - 1 ? "object-cover" : ""}
+                  className= {`object-cover`}
               />
             </div>
             <h2 className={`text-lg font-bold leading-8 mb-2`}>{p.title}</h2>
-            <p>{p.description}</p>
+       
+            <p className={``}>{p.description}</p>
             <span className="flex items-center gap-5 my-3">
               <span>{p.start_d}</span>
               الى
